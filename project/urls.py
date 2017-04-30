59bb194429c00b068df19575e03192382e15aad8
Kerwin Jorbina <kerwin.jorbina@gmail.com>
2017-05-01 02:45:55 +0300

    forecasting urls

--- project/urls.py
+++ project/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
 	url(r'^services/', include('services.urls')),
     url(r'^logs/', include('logs.urls')),
+    url(r'^forecasting/', include('forecasting.urls')),
     url(r'^admin/', admin.site.urls),
 ]
